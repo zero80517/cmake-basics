83092a05749ab8dba6f75ee67436d14bbe9274be
Emil Gilmkhanov <zero80517@gmail.com>
2022-08-27 15:10:38 +0300

    Added openmp-executable template

---
+++ cmake-templates/openmp-executable/README.md
@@ -0,0 +1,3 @@
+# openmp-executable
+
+Simple example of using CMake to compile openmp application.
---
+++ cmake-templates/openmp-executable/hello.cpp
@@ -0,0 +1,16 @@
+﻿#include <omp.h>
+#include <cstdio>
+
+int main() {
+  // This code is executed by 1 thread
+  const int nt = 8;
+  omp_set_num_threads(nt);
+  printf("OpenMP with %d threads\n", nt);
+
+  #pragma omp parallel
+  { // This code is executed in parallel
+    // by multiple threads
+    printf("Hello World from thread %d with %d num threads\n",
+           omp_get_thread_num(), omp_get_max_threads());
+  }
+}
